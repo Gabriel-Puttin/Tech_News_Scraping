d0d389d02e4888e68a115b03ca7ef5dde80b34fe
Gabriel Puttin <gabrielputtin@hotmail.com>
2023-04-14 18:20:02 -0300

    Feat: build top 5 categories

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,14 @@
-# Requisito 10
+from collections import Counter
+from tech_news.database import find_news
+
+
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    content = find_news()
+    categories = [news["category"] for news in content]
+    c = Counter(categories)
+    sort_by_category = sorted(c.most_common(5), key=lambda item: item[0])
+    sort_by_common = sorted(
+        sort_by_category, key=lambda item: item[1], reverse=True
+    )
+    most_common_categories = [category for category, _num in sort_by_common]
+    return most_common_categories
