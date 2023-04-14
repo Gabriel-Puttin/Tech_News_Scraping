ce7143683c936c1ea7106069b01edc2a414e44f1
Gabriel Puttin <gabrielputtin@hotmail.com>
2023-04-14 17:14:31 -0300

    Feat: build search_by_date func

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,18 +1,28 @@
 from tech_news.database import search_news
+from datetime import datetime
 
 
-def search_by_title(title: str):
-    query = {
-        "title": {"$regex": title.lower()}
-    }
-    content = search_news(query)
-
+def convert_to_tuple(content: list[dict]):
     return [(news["title"], news["url"]) for news in content]
 
 
-# Requisito 8
+def search_by_title(title: str):
+    query = {"title": {"$regex": title.lower()}}
+    content = search_news(query)
+
+    return convert_to_tuple(content)
+
+
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        new_date = datetime.strptime(date, "%Y-%m-%d").strftime("%d/%m/%Y")
+        query = {"timestamp": new_date}
+        content = search_news(query)
+
+    except (ValueError):
+        raise ValueError("Data inválida")
+
+    return convert_to_tuple(content)
 
 
 # Requisito 9
