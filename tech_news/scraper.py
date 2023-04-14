83059c4425e5a4f04135171594be4b6ec0933745
Gabriel Puttin <gabrielputtin@hotmail.com>
2023-04-14 15:38:27 -0300

    Feat: build get_tech_news func

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,7 @@
 import requests
 import time
 from bs4 import BeautifulSoup
+from tech_news.database import create_news
 
 
 def formated_str(phrase: str) -> str:
@@ -40,7 +41,6 @@ def scrape_next_page_link(html_content):
     return next_page_link["href"] if next_page_link else None
 
 
-# Requisito 4
 def scrape_news(html_content):
     soup = BeautifulSoup(html_content, "html.parser")
     news_report = dict()
@@ -65,6 +65,21 @@ def scrape_news(html_content):
     return news_report
 
 
-# Requisito 5
 def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+    news = []
+    blog = "https://blog.betrybe.com/"
+    html_blog = fetch(blog)
+    news_links = scrape_updates(html_blog)
+    next_page = scrape_next_page_link(html_blog)
+
+    while amount > len(news_links):
+        html_content_next_page = fetch(next_page)
+        news_links_next_page = scrape_updates(html_content_next_page)
+        news_links.extend(news_links_next_page)
+        next_page = scrape_next_page_link(html_content_next_page)
+
+    for link in news_links[:amount]:
+        news.append(scrape_news(fetch(link)))
+
+    create_news(news)
+    return news
